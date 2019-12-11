afdbdde1d7024266d2e078f57c16255d227f64fa
arttui <arttui@metropolia.fi>
2019-12-11 10:53:36 +0200

    Viimeinen committi

--- app/src/main/java/com/example/sleeptrackeralpha/MainActivity.java
+++ app/src/main/java/com/example/sleeptrackeralpha/MainActivity.java
@@ -45,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
                 int h   = (int)(time /3600000);
                 int m = (int)(time - h*3600000)/60000;
                 int s= (int)(time - h*3600000- m*60000)/1000 ;
-                String t = (h < 10 ? "0"+h: h)+":"+(m < 10 ? "0"+m: m)+":"+ (s < 10 ? "0"+s: s);
-                chronometer.setText(t);
+                String tunnit = (h < 10 ? "0"+h: h)+":"+(m < 10 ? "0"+m: m)+":"+ (s < 10 ? "0"+s: s);
+                chronometer.setText(tunnit);
             }
         });
         ajastin.setBase(SystemClock.elapsedRealtime());
